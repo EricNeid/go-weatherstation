33d36f2090e5a4dfb30e3f057002e14def3105cd
Eric Neidhardt <eric.neidhardt@gmail.com>
2020-06-27 20:04:49 +0200

    Refactor

--- cmd/weatherstation/main.go
+++ cmd/weatherstation/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/EricNeid/go-weatherstation"
 	"github.com/EricNeid/go-weatherstation/res"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 )
+
+type args struct {
+	fullscreen bool
+	imageDir   string
+	keyFile    string
+}
 
 const city = "Berlin"
 
@@ -24,4 +34,20 @@ func main() {
 
 	app := weatherstation.NewApp(a, w, city, args.keyFile, args.imageDir)
 	app.Start()
+}
+
+func parseArgs() args {
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	var args args
+	flag.BoolVar(&args.fullscreen, "fullscreen", false, "show app in fullscreen mode")
+	flag.StringVar(&args.imageDir, "images", "images", "directory, containing images for the screensave")
+	flag.StringVar(&args.keyFile, "key", "api.key", "file, containing api key for openweather")
+
+	flag.Parse()
+
+	return args
 }
